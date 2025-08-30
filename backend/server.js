69f81d6f484801149749ab4403f777cbc5212cf9
MULLAZMUSH <mullaz1@outlook.com>
2025-08-30 02:49:22 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -54,12 +54,12 @@ app.use((err, req, res, next) => {
 
 // 🔹 MongoDB Connection
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/victors-assembly-church';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://mullaz1_db_user:bJO2XJLIXjByePXF@cluster0.qebzav8.mongodb.net/victors-assembly-church?retryWrites=true&w=majorit';
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('✅ MongoDB connected');
+    console.log('✅ Connected to MongoDB Atlas');
     app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
